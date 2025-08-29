5a5ac5b4be7fbb2c5f899958f027901ffc2a7bb1
coderEdu <educoder78@gmail.com>
2025-08-29 03:21:47 -0300

    adv headers logged user logic

--- frontend/gestion-libros-react/src/components/header/Header.tsx
+++ frontend/gestion-libros-react/src/components/header/Header.tsx
@@ -1,12 +1,16 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 import './Header.css'
 
 function Header() {
-  return (
-    <div className='header'>
-      <h2>Sistema de Gestión de Libros</h2>
-      <h6>gtpanteraxtr@gmail.com 💚</h6>
-    </div>
-  )
+    const {userLoggedIn} = useContext(AuthContext);
+
+    return (
+        <div className='header'>
+        <h2>Sistema de Gestión de Libros</h2>
+        {userLoggedIn ? <h6>gtpanteraxtr@gmail.com 💚</h6> : null}
+        </div>
+    )
 }
 
 export default Header
